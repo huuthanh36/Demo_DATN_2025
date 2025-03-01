9c7d93fd1d4ab6ad9ce4437a77aab70467ee3f7e
Huy Doan <doanvanhuydhdtu@gmail.com>
2025-03-01 16:06:06 +0700

    initial project

---
+++ team30/src/main/java/com/datn/Team30Application.java
@@ -0,0 +1,13 @@
+package com.datn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team30Application.class, args);
+	}
+
+}
